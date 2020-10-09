3f91b3679d6d51bbd1b0a4827edf3e551f299dc8
“dineshega” <dinesh15.ega@gmail.com>
2020-10-09 09:47:27 +0530

    Added: Check emp present or absent

--- Employee.sh
+++ Employee.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
+#Welcome to Employee Wage Computation Program
+
 echo "Welcome to Employee Problem"
+
+isPresent=1
+randomCheck=$((RANDOM%2))
+if [ $randomCheck -eq $isPresent ]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
