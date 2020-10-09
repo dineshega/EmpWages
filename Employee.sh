9518c04b0f1164f7c63c85417a6c4b362af6b322
“dineshega” <dinesh15.ega@gmail.com>
2020-10-09 10:02:27 +0530

    Added: Case Statement

--- Employee.sh
+++ Employee.sh
@@ -8,16 +8,13 @@ isFullTime=1
 isPartTime=2
 empRatePerHour=20
 randomCheck=$((RANDOM%3))
-if [ $randomCheck -eq $isFullTime ]
-then
-	empHrs=8
-	
-elif [ $randomCheck -eq $isPartTime ]
-then
-	empHrs=4
-	
-else
-	empHrs=0
-fi
+case $randomCheck in
+	$isFullTime )
+			empHrs=8;;
+	$isPartTime )
+			empHrs=4;;
+	* )
+			empHrs=0;;
+esac
 salary=$(($empRatePerHour*$empHrs))
 echo "your one day salary : $salary"
