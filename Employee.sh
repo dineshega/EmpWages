23264039eb4d97ccc869cd59f0ad104ee31ca930
“dineshega” <dinesh15.ega@gmail.com>
2020-10-09 10:11:56 +0530

    Added: cal wages for total working hrs or day is reached

--- Employee.sh
+++ Employee.sh
@@ -4,27 +4,35 @@
 
 echo "Welcome to Employee Problem"
 
-isFullTime=1
-isPartTime=2
-empRatePerHour=20
-totalSalary=0
-numofworkingDays=20
+IS_FULL_TIME=1
+IS_PART_TIME=2
+EMP_RATE_PER_HOUR=20
+NO_OF_WORKING_DAYS=20
+MAX_HRS_IN_MONTH=160
 
-for(( day=1; day<=$numofworkingDays; day++ ))
+totalEmpHrs=0
+totalWorkingDays=0
+
+#for(( day=1; day<=$numofworkingDays; day++ ))
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NO_OF_WORKING_DAYS ]]
 do
+		((totalWorkingDays++))
+		
+		empCheck=$((RANDOM%3))
 
-		randomCheck=$((RANDOM%3))
-
-		case $randomCheck in
-			$isFullTime )
+		case $empCheck in
+			$IS_FULL_TIME )
 				empHrs=8;;
-			$isPartTime )
+			$IS_PART_TIME )
 				empHrs=4;;
 			* )
 				empHrs=0;;
 		esac
-		salary=$(($empRatePerHour*$empHrs))
-		totalSalary=$(($totalSalary+$salary))
+		
+		totalEmpHrs=$(($totalEmpHrs+$empHrs))
+		#salary=$(($empRatePerHour*$empHrs))
+		#totalSalary=$(($totalSalary+$salary))		
 done
 
-echo "totalSalary is: " $totalSalary
+		totalSalary=$(($EMP_RATE_PER_HOUR*$totalEmpHrs))
+		echo "totalSalary is: " $totalSalary 
